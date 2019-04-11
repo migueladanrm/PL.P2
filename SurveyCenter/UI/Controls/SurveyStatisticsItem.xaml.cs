a7473a58345a4acd633772f11e43bae3ee2d2d51
Miguel Rivas <migueladanrm@outlook.com>
2019-04-10 21:18:32 -0600

    Listo para entrega.

---
+++ SurveyCenter/UI/Controls/SurveyStatisticsItem.xaml.cs
@@ -0,0 +1,43 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace SurveyCenter.UI.Controls
+{
+    /// <summary>
+    /// Lógica de interacción para SurveyStatisticsItem.xaml
+    /// </summary>
+    public partial class SurveyStatisticsItem : UserControl
+    {
+        public SurveyStatisticsItem()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        public SurveyStatisticsItem(JObject surveyItem, List<int> values)
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            SeriesCollection = new SeriesCollection();
+
+            var content = (JArray)surveyItem["content"];
+
+            for (int i = 0; i < content.Count; i++) {
+                SeriesCollection.Add(new PieSeries {
+                    Title = (string)content[i],
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(values[i]) },
+                    DataLabels = true
+                });
+            }
+
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+    }
+}
